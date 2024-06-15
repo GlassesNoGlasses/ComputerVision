0baff1214a40792a1dfc9ee4ddc14abc98d9526d
Neil Wang <neilywang3@gmail.com>
2024-06-14 18:20:50 -0700

    added basic drawing

--- finger-painting/handTracking.py
+++ finger-painting/handTracking.py
@@ -2,6 +2,7 @@
 import cv2
 import numpy as np
 import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
 
 # frame dimensions
 FRAME_WIDTH = 1280
@@ -20,7 +21,10 @@ mpDraw = mp.solutions.drawing_utils
 
 # image
 BLACK_IMG = np.zeros((FRAME_HEIGHT, FRAME_WIDTH, 3), dtype = np.uint8)
+DRAW_IMG = np.ones((FRAME_HEIGHT, FRAME_WIDTH, 3), dtype = np.uint8) * 255
 
+# colors
+GREEN_COLOR = (0, 255, 0)
 
 while cap.isOpened():
 
@@ -38,12 +42,17 @@ while cap.isOpened():
     # if hands are detected, draw the landmarks
     if hands_results.multi_hand_landmarks:
         for hand_landmarks in hands_results.multi_hand_landmarks:
+            index_finger_tip = hand_landmarks.landmark[8]
+            print(index_finger_tip)
+            print(index_finger_tip.x, index_finger_tip.y)
             mpDraw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
             mpDraw.draw_landmarks(black_img, hand_landmarks, mpHands.HAND_CONNECTIONS)
+            
+            DRAW_IMG = cv2.circle(DRAW_IMG, (int(index_finger_tip.x * 1000), int(index_finger_tip.y * 1000)), radius=2, color=GREEN_COLOR, thickness=-1)
     
     # display the frame
     cv2.imshow('frame', frame)
-    cv2.imshow('black_img', black_img)
+    cv2.imshow('DRAW_IMG', DRAW_IMG)
 
 
     # break the loop if 'q' is pressed
