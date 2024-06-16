cc40ca89876e7eec71194da36d64a2c99488b2d3
Neil Wang <neilywang3@gmail.com>
2024-06-16 11:01:16 -0700

    added csv support

--- face-recognition/recognition.py
+++ face-recognition/recognition.py
@@ -2,18 +2,14 @@
 import cv2
 import numpy as np
 import os
+import csv
 from PIL import Image
-
-# Keyboard inputs
-ESC = 27
 
 # saving face image logic
 MAX_FACES = 10
 THRESHOLD = (500, 500)
-
-## make faces directory to store faces if it doesn't exist
-if not os.path.exists('./faces'):
-    os.makedirs('faces')
+USER_FACES_DIR = './faces'
+TEST_FACES_DIR = './test_faces'
 
 # camera
 camera = cv2.VideoCapture(1)
@@ -23,6 +19,19 @@ face_classifier = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
+def initializeFiles():
+    ''' Initialize the files/dirs for the face recognition system.'''
+
+    # make users face directory if it doesn't exist
+    if not os.path.exists(USER_FACES_DIR):
+        os.makedirs('faces')
+    
+    # make the csv file to store the face encodings
+    if not os.path.exists('face_encodings.csv'):
+        f = open('face_encodings.csv', 'w')
+        f.close()
+
+
 def padRectangle(x, y, w, h, size=500):
     ''' Pad the rectangle around the face to be 500x500.'''
 
@@ -30,6 +39,7 @@ def padRectangle(x, y, w, h, size=500):
     height_padding = size - h
 
     return (x, y, w + width_padding, h + height_padding)
+
 
 def getFaceCoords(gray_frame):
     ''' Outlines the face in the frame if it exists.
@@ -87,7 +97,7 @@ def getFaces():
             num_faces += 1
             x, y, w, h = coords
             face_frame = frame[y:y+h, x:x+w]
-            cv2.imwrite(f'./faces/face_{num_faces}.jpg', face_frame)
+            cv2.imwrite(f'{USER_FACES_DIR}/user_{num_faces}.jpg', face_frame)
         
         # stop the camera after 10 faces
         if (num_faces >= MAX_FACES):
@@ -101,11 +111,49 @@ def getFaces():
         cv2.imshow('Face Feed', frame)
 
 
+def writeFaceEncodings(csv_file_path='face_encodings.csv'):
+    ''' Write the face encodings to the csv file.'''
+
+    # open the csv file
+    with open(csv_file_path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        fields = ['file_name', 'is_user', 'path']
+        writer.writerow(fields)
+
+        # loop through the user faces
+        for i in range(1, MAX_FACES + 1):
+            face_path = f'{USER_FACES_DIR}/user_{i}.jpg'
+
+            # write the face encoding to the csv file
+            writer.writerow([f'user_{i}.jpg', 1, face_path])
+        
+        # loop through the non-user faces
+        directory = os.fsencode(TEST_FACES_DIR)
+            
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+
+            if filename.endswith(".jpeg") or filename.endswith(".jpg") or filename.endswith(".png"):
+                file_path = os.path.join(directory.decode(), filename)
+                writer.writerow([filename, 0, file_path])
+
+
+        f.close()
+    print("Wrote face encodings to csv file!")
+
 # main function
 def main():
+    ''' Main function to run the face recognition system.'''
+
+    # initialize the files/dirs needed
+    initializeFiles()
+
     # get faces from the camera feed
     getFaces()
     print("Got all faces!")
+    
+    # write the face encodings to the csv file
+    writeFaceEncodings()
 
 
 main()
